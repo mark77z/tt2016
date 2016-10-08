e612ea1ed944b160f7fa3ecb01dac3ccbf3797d7
Gogs <gogs@fake.local>
2016-10-08 17:30:05 -0500

    Añadidas notificaciones para la administracion de las solicitudes de registro

--- routers/admin/applications.go
+++ routers/admin/applications.go
@@ -54,6 +54,8 @@ func ActivateUser(ctx *context.Context) {
 	}
 	log.Trace("Account profile updated by admin (%s): %s", ctx.User.Name, u.Name)
 
+	models.SendApprovedAccountMail(ctx.Context, u)
+
 	ctx.Flash.Success(ctx.Tr("admin.users.update_profile_success"))
 	ctx.JSON(200, map[string]interface{}{
 		"redirect": setting.AppSubUrl + "/admin/applications",
@@ -86,101 +88,10 @@ func DeleteApplicationUser(ctx *context.Context) {
 	}
 	log.Trace("Account deleted by admin (%s): %s", ctx.User.Name, u.Name)
 
+	models.SendDeniedAccountMail(ctx.Context, u)
+
 	ctx.Flash.Success(ctx.Tr("admin.users.deletion_success"))
 	ctx.JSON(200, map[string]interface{}{
 		"redirect": setting.AppSubUrl + "/admin/applications",
 	})
 }
-
-/* 
-func EditUserPost(ctx *context.Context, form auth.AdminEditUserForm) {
-	ctx.Data["Title"] = ctx.Tr("admin.users.edit_account")
-	ctx.Data["PageIsAdmin"] = true
-	ctx.Data["PageIsAdminUsers"] = true
-
-	u := prepareUserInfo(ctx)
-	if ctx.Written() {
-		return
-	}
-
-	if ctx.HasError() {
-		ctx.HTML(200, USER_EDIT)
-		return
-	}
-
-	fields := strings.Split(form.LoginType, "-")
-	if len(fields) == 2 {
-		loginType := models.LoginType(com.StrTo(fields[0]).MustInt())
-		loginSource := com.StrTo(fields[1]).MustInt64()
-
-		if u.LoginSource != loginSource {
-			u.LoginSource = loginSource
-			u.LoginType = loginType
-		}
-	}
-
-	if len(form.Password) > 0 {
-		u.Passwd = form.Password
-		u.Salt = models.GetUserSalt()
-		u.EncodePasswd()
-	}
-
-	u.LoginName = form.LoginName
-	u.FullName = form.FullName
-	u.Email = form.Email
-	u.Website = form.Website
-	u.Location = form.Location
-	u.MaxRepoCreation = form.MaxRepoCreation
-	u.IsActive = form.Active
-	u.IsAdmin = form.Admin
-	u.AllowGitHook = form.AllowGitHook
-	u.AllowImportLocal = form.AllowImportLocal
-	u.ProhibitLogin = form.ProhibitLogin
-
-	if err := models.UpdateUser(u); err != nil {
-		if models.IsErrEmailAlreadyUsed(err) {
-			ctx.Data["Err_Email"] = true
-			ctx.RenderWithErr(ctx.Tr("form.email_been_used"), USER_EDIT, &form)
-		} else {
-			ctx.Handle(500, "UpdateUser", err)
-		}
-		return
-	}
-	log.Trace("Account profile updated by admin (%s): %s", ctx.User.Name, u.Name)
-
-	ctx.Flash.Success(ctx.Tr("admin.users.update_profile_success"))
-	ctx.Redirect(setting.AppSubUrl + "/admin/users/" + ctx.Params(":userid"))
-}
-
-func DeleteUser(ctx *context.Context) {
-	u, err := models.GetUserByID(ctx.ParamsInt64(":userid"))
-	if err != nil {
-		ctx.Handle(500, "GetUserByID", err)
-		return
-	}
-
-	if err = models.DeleteUser(u); err != nil {
-		switch {
-		case models.IsErrUserOwnRepos(err):
-			ctx.Flash.Error(ctx.Tr("admin.users.still_own_repo"))
-			ctx.JSON(200, map[string]interface{}{
-				"redirect": setting.AppSubUrl + "/admin/users/" + ctx.Params(":userid"),
-			})
-		case models.IsErrUserHasOrgs(err):
-			ctx.Flash.Error(ctx.Tr("admin.users.still_has_org"))
-			ctx.JSON(200, map[string]interface{}{
-				"redirect": setting.AppSubUrl + "/admin/users/" + ctx.Params(":userid"),
-			})
-		default:
-			ctx.Handle(500, "DeleteUser", err)
-		}
-		return
-	}
-	log.Trace("Account deleted by admin (%s): %s", ctx.User.Name, u.Name)
-
-	ctx.Flash.Success(ctx.Tr("admin.users.deletion_success"))
-	ctx.JSON(200, map[string]interface{}{
-		"redirect": setting.AppSubUrl + "/admin/users",
-	})
-}
-*/
